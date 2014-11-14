995cf861940c838cf540b2752f0fdb6b3d6b0aed
Phani <raghavendra.rupakula@cdk.com>
2014-11-14 15:39:24 +0530

    more code

--- Program.cs
+++ Program.cs
@@ -10,13 +10,46 @@ namespace CodingBatSolutions
         // WarmUp-1
         static void Main(string[] args)
         {
-            bool res = makes10(9, 10);
-                
+            string res = missingChar("kitten",1);
             Console.WriteLine(res.ToString());
             Console.ReadLine();
         }
 
+        public static string missingChar(string str, int n)
+        {
+            string newStr = string.Empty;
+            if (!string.IsNullOrEmpty(str))
+            {
+                newStr = str.Remove(n);
 
+            }
+            return newStr;
+
+        }
+
+        public static string notString(string str)
+        {
+            if (str.Length >= 3)
+            {
+                if (str.Substring(0, 3) == "not")
+                    return str;
+
+            }
+            return "not " + str;
+
+        }
+
+        public static bool posNeg(int a, int b, bool negative)
+        {
+            if (negative)
+            {
+                return (a < 0 && b < 0);
+            }
+            else
+            {
+                return ((a < 0 && b > 0) || (a > 0 && b < 0));
+            }
+        }
         public static bool makes10(int a, int b)
         {
             return ((a == 10 || b == 10) || (a + b == 10));
